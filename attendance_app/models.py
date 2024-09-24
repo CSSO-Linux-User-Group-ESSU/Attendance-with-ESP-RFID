d2c0e74a6034fec723cf0c8737f983cf225f6700
Gabrielkaos <gabrielmontes155@gmail.com>
2024-09-24 09:35:25 +0800

    second_push

--- attendance_app/models.py
+++ attendance_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    card_uid = models.CharField(max_length=100)
+
+    last_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    student_id = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}, {self.middle_name}"
+
+
--- attendance_app/views.py
+++ attendance_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+import openpyxl
+from django.shortcuts import render
+from .forms import UploadFileForm
+from .models import Student
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            excel_file = request.FILES['files']
+            wb = openpyxl.load_workbook(excel_file)
+            sheet = wb.active
+
+            for row in sheet.iter_rows(min_row=2, values_only=True):
+                card_uid, last_name, first_name, middle_name, student_id = row
+
+                if not Student.objects.filter(card_uid=card_uid).exists():
+                    Student.objects.create(card_uid=card_uid, last_name=last_name, first_name=first_name, middle_name=middle_name, student_id=student_id)
+
+            return render(request, 'attendance_app/success.html')
+    else:
+        form = UploadFileForm()
+
+    return render(request, 'attendance_app/upload.html', {'form': form})
