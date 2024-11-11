0c84ed83683c556076dc9d4c105eaaf05f527212
Gabrielkaos <gabrielmontes155@gmail.com>
2024-11-11 18:45:09 +0800

    added fields to the Device model

--- attendance_app/models.py
+++ attendance_app/models.py
@@ -15,7 +15,8 @@ class Device(models.Model):
     token = models.ForeignKey(SecurityToken, on_delete=models.CASCADE)
     ssid = models.CharField(max_length=100)
     password_to_ssid = models.CharField(max_length=200)
-    url = models.CharField(max_length=300)
+    url = models.URLField()
+    ip_address = models.GenericIPAddressField(null=True,blank=True)
     status = models.BooleanField()
 
     def __str__(self):
