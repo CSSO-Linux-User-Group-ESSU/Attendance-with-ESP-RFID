5464c982a8578126797a2e8a8c7e1b70b6cd7734
Gabrielkaos <gabrielmontes155@gmail.com>
2025-02-27 17:07:40 +0800

    reverted to sqlite

--- attendance/settings.py
+++ attendance/settings.py
@@ -79,25 +79,25 @@ WSGI_APPLICATION = 'attendance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ATTENDANCE_DB',               # Replace with your database name
-        'USER': 'root',                        # Replace with your database username
-        'PASSWORD': 'gabmontes',               # Replace with your database password
-        'HOST': 'localhost',                   # Replace with your database host, often 'localhost'
-        'PORT': '3306',                        # Default MariaDB/MySQL port
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ATTENDANCE_DB',               # Replace with your database name
+#         'USER': 'root',                        # Replace with your database username
+#         'PASSWORD': 'gabmontes',               # Replace with your database password
+#         'HOST': 'localhost',                   # Replace with your database host, often 'localhost'
+#         'PORT': '3306',                        # Default MariaDB/MySQL port
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
 
 
 
