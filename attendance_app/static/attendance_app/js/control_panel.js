008e2bf7669fdf8449e44e62e681ed25225d5948
EspaMC16 <markchristianespanol@gmail.com>
2025-03-16 13:20:59 +0800

    ok na to

--- attendance_app/static/attendance_app/js/control_panel.js
+++ attendance_app/static/attendance_app/js/control_panel.js
@@ -7,3 +7,15 @@ navLinks.forEach(link => {
         link.classList.add('nav-active');
     }
 });
+
+/* Open side bar */
+const sidebar = document.querySelector(".sidebar");
+
+function showSidebar() {
+    sidebar.style.display = "flex";
+}
+
+/* Close side bar */
+function hideSidebar() {
+    sidebar.style.display = "none";
+}
